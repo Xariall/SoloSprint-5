55492adaaa6f5e117de361d37203b1b2622896af
Asanali Esmagambetov <supersanin45@gmail.com>
2023-04-17 13:09:31 +0600

    Add js for project

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
 from .models import *
 
 # Create your views here.
 def store(request):
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+		cartItems = order['get_cart_items']
+
 	products = Product.objects.all()
-	context = {'products' : products}
+	context = {'products':products, 'cartItems':cartItems}
+
 	return render(request, 'main/store.html', context)
 
 def cart(request):
 	if request.user.is_authenticated:
 		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete = False)
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
 	else:
+		#Create empty cart for now for non-logged in user
+		try:
+			cart = json.loads(request.COOKIES['cart'])
+		except:
+			cart = {}
+			print('CART:', cart)
+
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
-	context = {'items':items, 'order':order}
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+		cartItems = order['get_cart_items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'main/cart.html', context)
 
 def checkout(request):
