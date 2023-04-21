08243e7f9ba7ce620b65877c6fa9764452c620db
Baktiyar Zhaksybay <baktyar100@gmail.com>
2023-04-21 22:38:00 +0600

    logotip for project

---
+++ shop/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+# 
+urlpatterns = [
+    path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+]
