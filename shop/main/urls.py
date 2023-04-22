dd647d4c223f48fec2adb1b84a19bb32ab9993f2
Asanali Esmagambetov <supersanin45@gmail.com>
2023-04-22 11:19:25 +0600

    login register

--- shop/main/urls.py
+++ shop/main/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+    
+    path('login/', views.customer_login, name="login"),
+    path('register/', views.customer_register, name="register"),
 ]
