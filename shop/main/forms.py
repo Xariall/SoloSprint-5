e45159182b3865c3eb2ae9643d448c7be0ec4791
Baktiyar Zhaksybay <baktyar100@gmail.com>
2023-04-26 21:23:29 +0600

    adding shoe characteristics

---
+++ shop/main/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Customer, User
+
+
+class CustomerRegistrationForm(UserCreationForm):
+    name = forms.CharField(max_length=200)
+
+    class Meta:
+        model = Customer
+        fields = ('name', 'email', 'password1', 'password2')
+
+class CustomerLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+#BAKTIyAR
